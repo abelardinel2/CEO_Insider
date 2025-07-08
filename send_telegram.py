530d75a3cf2ad0093103dbfaca8aeb668ea9d600
abelardinel2 <ameliabelardinelli@gmail.com>
2025-07-08 14:35:18 -0400

    Update send_telegram.py

--- send_telegram.py
+++ send_telegram.py
@@ -1,7 +1,6 @@
 import os
 import requests
 
-
 def send_alert(ticker, owner, trade_type, amount, bias, link):
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
@@ -21,4 +20,4 @@ def send_alert(ticker, owner, trade_type, amount, bias, link):
     if response.status_code == 200:
         print(f"✅ Alert sent for {ticker}")
     else:
-        print(f"❌ Telegram send failed: {response.status_code} | {response.text}")
+        print(f"❌ Telegram send failed: {response.text}")
